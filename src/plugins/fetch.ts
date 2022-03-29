a46ded61e4f8f9ebe9252d46e319bd847033c4a4
Alexis-LB54 <alexis@lebail.eu>
2022-03-29 16:54:20 +0200

    ajout login et modif consult

--- src/plugins/fetch.ts
+++ src/plugins/fetch.ts
@@ -2,13 +2,17 @@
 import type { User } from "../types/userType";
 
 export default async function fetchLogin(user: User) {
-  await fetch("https://apidoctor.quidam.re/api/login_check", {
+  let response = await fetch("https://apidoctor.quidam.re/api/login_check", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(user),
-  });
+  })
+    .then((r) => r.json())
+    .catch();
+    console.log("utilisateur", user, "token", response);
+    
 }
 
 export async function fetchConsults(token: string) {
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,24 @@
 <template>
-  <div>
+  <!-- <div>
     <Login @form-submit="FormSubmit" />
+  </div> -->
+  
+    <div>
+    <form @submit.prevent="FormSubmit">
+
+      <label for="username">username</label>
+      <input type="text" v-model="user.username" id="username"/>
+
+      <label for="password">password</label>
+      <input type="password" v-model="user.password" id="password"/>
+
+      <input type="submit" value="Se connecter" />
+    </form>
   </div>
 </template>
 
 <script setup>
-import Login from "../components/forms/Login.vue";
+// import Login from "../components/forms/Login.vue";
 import { ref } from "@vue/reactivity";
 import fetchLogin from "../plugins/fetch";
 
